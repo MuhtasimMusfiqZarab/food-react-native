c42e9cf4144a11b04d534424f6c761ec20c5f1d4
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2019-09-13 12:21:15 +0600

    initial search

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import yelp from "../api/yelp";
@@ -10,14 +10,14 @@ const SearchScreen = () => {
   const [errorMessage, setErrorMessage] = useState("");
 
   //API search
-  const searchAPI = async () => {
+  const searchAPI = async searchTerm => {
     try {
       const response = await yelp.get("/search", {
         //parameters to customize the search we make
         params: {
           //appending to the requested url
           limit: 50,
-          term,
+          term: searchTerm,
           location: "san jose"
         }
       });
@@ -28,13 +28,18 @@ const SearchScreen = () => {
     }
   };
 
+  //run once when the component is rendedred the first time (componentDidMount)
+  useEffect(() => {
+    searchAPI("pasta");
+  }, []);
+
   return (
     <View>
       {/* must provide both the term and onTermChange callback to change it */}
       <SearchBar
         term={term}
         onTermChange={newValue => setTerm(newValue)}
-        onTermSubmit={searchAPI}
+        onTermSubmit={() => searchAPI(term)} //term is from the state
       />
       {errorMessage ? <Text>Found :{results.length}</Text> : null}
       <Text>Results Found :{results.length}</Text>
