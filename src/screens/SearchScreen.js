ce49a32478b75d566e4033e140f79e838ca93588
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2019-09-13 11:54:06 +0600

    making api search request

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,9 +1,27 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  //save api reasults here
+  const [results, setResults] = useState([]);
+
+  //API search
+  const searchAPI = async () => {
+    const response = await yelp.get("/search", {
+      //parameters to customize the search we make
+      params: {
+        //appending to the requested url
+        limit: 50,
+        term,
+        location: "san jose"
+      }
+    });
+    //saving to results array
+    setResults(response.data.businesses);
+  };
 
   return (
     <View>
@@ -11,10 +29,10 @@ const SearchScreen = () => {
       <SearchBar
         term={term}
         onTermChange={newValue => setTerm(newValue)}
-        onTermSubmit={() => console.log("Term is submitted")}
+        onTermSubmit={searchAPI}
       />
       <Text> Search Screen</Text>
-      <Text>{term}</Text>
+      <Text>Found :{results.length}</Text>
     </View>
   );
 };
