b25e13c6e445a7fa6427a1406b0ce249a9ba9978
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2019-09-13 17:30:04 +0600

    fetching single restaurent data

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,9 +1,25 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
+
+import yelp from "../api/yelp";
 
 // this navigation is passed when navigated from ResultList
 const ResultsShowScreen = ({ navigation }) => {
+  const [result, setResult] = useState(null); // we normally initialize null for upcoming receiving onject
   const id = navigation.getParam("id");
+  console.log(result);
+
+  //helper function to retrieve data
+  const getResult = async id => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+
+  //call this exactly once
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
   return (
     <View>
       <Text>{id}</Text>
