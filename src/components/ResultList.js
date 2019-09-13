e3c4e3143336e9d98affa3c978fb8f0a1d6b9c58
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2019-09-13 14:43:19 +0600

    FlatList rendering

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
 const ResultList = ({ title, results }) => {
   return (
     <View>
       <Text style={styles.titleStyle}>{title}</Text>
-      <Text>{results.length}</Text>
+      {/* showing products using ResultList */}
+      <FlatList
+        horizontal
+        data={results}
+        keyExtractor={result => result.id}
+        //renderItem has {item,index}
+        renderItem={({ item }) => {
+          return <Text>{item.name}</Text>;
+        }}
+      />
     </View>
   );
 };
